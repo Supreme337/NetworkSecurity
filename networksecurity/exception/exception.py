5fe699bf246a0192e66a62262823a62675946227
Supreme337 <harshmalik0034@gmail.com>
2025-09-07 21:11:41 +0530

    ETL Pipeline completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,6 +1,6 @@
 import sys
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details;sys):
+    def __init__(self,error_message,error_details,sys):
         self.error_message=error_message
         _,_,exc_tb=error_details.exc_info()
         self.lineno=exc_tb.tb_lineno

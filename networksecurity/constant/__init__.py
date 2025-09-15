62821fe596313a0b23beecf2f78856dcfbc41068
Supreme337 <harshmalik0034@gmail.com>
2025-09-15 23:33:04 +0530

    Data Validation pipeline done

--- networksecurity/constant/__init__.py
+++ networksecurity/constant/__init__.py
@@ -12,6 +12,8 @@ PIPELINE_NAME: str ="NetworkSecurity"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
 
+SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
+
 """Data Ingestion related constant start with DATA_INGESTION VAR NAME"""
 DATA_INGESTION_COLLECTION_NAME:str="NetworkSecurity"
 DATA-INGESTION_DATABASE_NAME:str:="Harsh"
@@ -19,3 +21,10 @@ DATA_INGESTION_DIR_NAME:"data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+"""Data Validation related constant start with DATA_VALIDATION_VAR_NAME"""
+DATA_VALIDATION_DIR_NAME:str="data_validation"
+DATA_VALIDATION_VALID_DIR:str="valid"
+DATA_VALIDATION_INVALID_DIR:str="invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"

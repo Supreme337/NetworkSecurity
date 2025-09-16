b1ab48abd57d20eb2a8397ac1238e82553e88487
Supreme337 <harshmalik0034@gmail.com>
2025-09-16 22:21:31 +0530

    Data Transformation pipeline done

--- networksecurity/constant/__init__.py
+++ networksecurity/constant/__init__.py
@@ -5,9 +5,9 @@ import sys
 
 """Defining common constant variables for training pipeline"""
 TARGET_COLUMN="Result"
-ARTIFACT_DIR: str ="Artifacts"
-FILE_NAME: str ="phishingData.csv"
-PIPELINE_NAME: str ="NetworkSecurity"
+ARTIFACT_DIR:str="Artifacts"
+FILE_NAME:str="phishingData.csv"
+PIPELINE_NAME:str="NetworkSecurity"
 
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
@@ -18,9 +18,9 @@ SCHEMA_FILE_PATH=os.path.join("data_schema","schema.yaml")
 DATA_INGESTION_COLLECTION_NAME:str="NetworkSecurity"
 DATA-INGESTION_DATABASE_NAME:str:="Harsh"
 DATA_INGESTION_DIR_NAME:"data_ingestion"
-DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
-DATA_INGESTION_INGESTED_DIR: str = "ingested"
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+DATA_INGESTION_FEATURE_STORE_DIR:str="feature_store"
+DATA_INGESTION_INGESTED_DIR:str="ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float=0.2
 
 """Data Validation related constant start with DATA_VALIDATION_VAR_NAME"""
 DATA_VALIDATION_DIR_NAME:str="data_validation"
@@ -28,3 +28,15 @@ DATA_VALIDATION_VALID_DIR:str="valid"
 DATA_VALIDATION_INVALID_DIR:str="invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+
+"""Data Transformation related constant start with DATA_TRANSFORMATION_VAR_NAME"""
+DATA_TRANSFORMATION_DIR_NAME:str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str="transformed_object"
+## kkn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
+    "missing_values":np.nan,
+    "n_neighbors":3,
+    "weights":"uniform"}
+DATA_TRANSFORMATION_TRAIN_FILE_PATH:str="train.npy"
+DATA_TRANSFORMATION_TEST_FILE_PATH:str="test.npy"
